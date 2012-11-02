62435dee53092bab4c7e905c3f0bb655febd880f
Niklas Hofer <niklas+dev@lanpartei.de>
2012-11-02 17:13:14 +0100

    apache's rewrite module has no conf, look for .load file

--- manifests/webserver/rewrite.pp
+++ manifests/webserver/rewrite.pp
@@ -1,7 +1,7 @@
 class rails::webserver::rewrite {
   # untested
   exec { 'a2enmod rewrite':
-    unless  => 'test -L /etc/apache2/mods-enabled/rewrite.conf',
+    unless  => 'test -L /etc/apache2/mods-enabled/rewrite.load',
     require => Package['apache2'],
     notify  => Service['apache2']
   }
