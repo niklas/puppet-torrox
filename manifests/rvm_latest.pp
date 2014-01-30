96c32ba23e53d8f7a87c2a2f90141e0a3d3840cc
Matthias Viehweger <kronn@kronn.de>
2014-01-30 19:10:12 +0100

    Add trailing commas to enhance future diffs

--- manifests/rvm_latest.pp
+++ manifests/rvm_latest.pp
@@ -2,7 +2,7 @@ class rails::rvm_latest {
   class { 'rvm':
     version     => latest,
     install_rvm => true,
-    require     => File['/etc/rvmrc']
+    require     => File['/etc/rvmrc'],
   }
 
   # we want to use application specific rvmrcs
@@ -11,13 +11,13 @@ class rails::rvm_latest {
     mode    => '0644',
     content => "umask g+w
 rvm_trust_rvmrcs_flag=1
-rvm_auto_reload_flag=2"
+rvm_auto_reload_flag=2",
   }
 
   # generate no docs for gems
   file { '/etc/gemrc':
     path    => '/etc/gemrc',
     mode    => '0644',
-    content => 'gem: --no-rdoc --no-ri'
+    content => 'gem: --no-rdoc --no-ri',
   }
 }
