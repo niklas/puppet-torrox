9a8c4a8e7c3da0dfd607d889fab5ce5900404e91
Matthias Viehweger <kronn@kronn.de>
2015-02-26 19:25:38 +0100

    Spec integration with rvm-module to install passenger

---
+++ spec/defines/passenger_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'rails::passenger' do
+  let(:facts) do
+    {
+      :rvm_installed => true
+    }
+  end
+  let(:title) { 'ruby-1.9.3-p194' }
+  let(:params) do
+    {
+      :passenger_version => '4.0.8',
+    }
+  end
+
+  it 'installs passenger' do
+    should contain_class('rvm::passenger::apache').
+      with_ruby_version('ruby-1.9.3-p194').
+      with_version('4.0.8')
+  end
+
+  it 'integrates with apache' do
+    should contain_package('apache2').with_ensure('present')
+    should contain_service('apache2').with_ensure('running')
+  end
+end
