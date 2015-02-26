f21c67c1521210c91083e8a741b933f7e3000105
Matthias Viehweger <kronn@kronn.de>
2015-02-26 19:24:32 +0100

    Spec more details of the app-skeleton

--- spec/defines/application_spec.rb
+++ spec/defines/application_spec.rb
@@ -33,4 +33,66 @@ describe 'rails::application' do
        with_ensure("directory").
        with_mode("0755")
   end
+
+  it 'ensures that crond is running' do
+    should contain_package('cron').with_ensure('installed')
+    should contain_service('crond').with_ensure('running')
+  end
+
+  it 'creates a directory for run-files' do
+    should contain_file('run-dir for heinz of testapp').
+      with_ensure("directory").
+      with_path("/home/heinz/projects/testapp/production/shared/pids").
+      with_owner("heinz").
+      with_group("root").
+      with_mode("0755")
+  end
+
+  it 'creates a normal capistrano-setup' do
+    should contain_file('/home/heinz/projects/testapp/production/releases').
+      with_ensure('directory').
+      with_owner("heinz").
+      with_group("heinz").
+      with_mode("0755")
+
+    should contain_file('/home/heinz/projects/testapp/production/shared').
+      with_ensure('directory').
+      with_owner("heinz").
+      with_group("heinz").
+      with_mode("0755")
+
+    should contain_file('/home/heinz/projects/testapp/production/shared/config').
+      with_ensure('directory').
+      with_owner("heinz").
+      with_group("heinz").
+      with_mode("0755")
+
+    should contain_file('/home/heinz/projects/testapp/production/shared/system').
+      with_ensure('directory').
+      with_owner("heinz").
+      with_group("heinz").
+      with_mode("0755")
+  end
+
+  it 'provides directories for logs' do
+    should contain_file("/home/heinz/projects/testapp/production/shared/log").
+      with_ensure('directory').
+      with_owner("heinz").
+      with_group("heinz").
+      with_mode("0755")
+
+    should contain_file("/home/heinz/projects/testapp/production/shared/log/rotated").
+      with_ensure('directory').
+      with_owner("heinz").
+      with_group("heinz").
+      with_mode("0755")
+
+    should contain_file('/home/heinz/projects/testapp/production/shared/log/production.log').
+      with_mode("0666")
+  end
+
+  it 'database.yml is initialized' do
+    should contain_file('/home/heinz/projects/testapp/production/shared/config/database.yml').
+      with_replace("false")
+  end
 end
